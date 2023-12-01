bd4224551231c67de3450077d83cd950cc60c891
ibrahem <ibrahem@altatweertech.com>
2023-12-01 16:39:07 +0200

    add-db

--- database/migrations/2023_11_16_161034_create_leagues_table.php
+++ database/migrations/2023_11_16_161034_create_leagues_table.php
@@ -4,7 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
@@ -14,6 +15,7 @@ return new class extends Migration {
             $table->id();
             $table->string('name')->unique();
             $table->bigInteger('user_id');
+            $table->bigInteger('page_id');
             $table->boolean('is_capten')->default(0);
             $table->boolean('is_spare')->default(0);
             $table->enum('status', ['league', 'kass'])->default('league');
